50d6f36215c1f94a29920dacc6d6a5ab369052f3
Pam Drouin <appnotifs@fastmail.com>
2025-08-19 21:44:34 -0500

    Initial commit

---
+++ src/_11ty/shortcodes/image-shortcode.js
@@ -0,0 +1,25 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes, classes, loading = "lazy") {
+  // Remove leading slash if it exists
+  src = src.startsWith("/") ? src.slice(1) : src;
+
+  let metadata = await Image(src, {
+    widths: [25, 320, 640, 960, 1200, 1800, 2400],
+    formats: ["webp", "jpeg"],
+    urlPath: "/assets/img/",
+    outputDir: "_site/assets/img/",
+  });
+
+  let imageAttributes = {
+    class: classes,
+    alt,
+    sizes,
+    loading,
+    decoding: "async",
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
+module.exports = imageShortcode;
